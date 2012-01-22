9cabdd14165e5ed1bfe4b309bc87eabd7b6326b0
Fred Emmott <mail@fredemmott.co.uk>
2012-01-22 20:55:02 +0000

    phrasing changes for regexp spec

--- spec/regexp_spec.rb
+++ spec/regexp_spec.rb
@@ -2,29 +2,29 @@ require 'spec_helper'
 require 'sidewalk/regexp'
 
 describe Sidewalk::Regexp do
-  it 'supports named captures' do
+  it 'should support named capture' do
     reg = Sidewalk::Regexp.new('foo(?<bar>\d+)baz')
     match = reg.match 'foo123baz'
     match[:bar].should == '123'
   end
 
-  it 'supports listing names' do
+  it 'should support listing names' do
     reg = Sidewalk::Regexp.new('foo(?<bar>\d+)baz')
     match = reg.match 'foo123baz'
     match.names.should include 'bar'
   end
 
-  it 'supports #post_match' do
+  it 'should support #post_match' do
     reg = Sidewalk::Regexp.new('herpity')
     match = reg.match 'herpity derpity'
     match.post_match.should == ' derpity'
   end
 
-  it 'supports trivial matches' do
+  it 'should support trivial matches' do
     (Sidewalk::Regexp.new('foo') =~ 'foo').should == 0
   end
 
-  it 'supports trivial non-matches' do
+  it 'should support trivial non-matches' do
     (Sidewalk::Regexp.new('foo') =~ 'bar').should be_nil
   end
 end
