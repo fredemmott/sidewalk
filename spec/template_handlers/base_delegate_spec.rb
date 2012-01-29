2295d640548a234291f85f0b90dcbbf091759c34
Fred Emmott <mail@fredemmott.co.uk>
2012-01-29 11:07:47 +0000

    add testing of BaseDelegate

---
+++ spec/template_handlers/base_delegate_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+require 'sidewalk/template_handlers/base_delegate'
+
+describe Sidewalk::TemplateHandlers::BaseDelegate do
+  it 'copies all instance variables from the controller class' do
+    controller = Object.new
+    controller.instance_eval do
+      @foo = :bar
+      @herp = :derp
+    end
+
+    delegate = Sidewalk::TemplateHandlers::BaseDelegate.new(controller)
+
+    foo = delegate.instance_eval{ @foo }
+    herp = delegate.instance_eval{ @herp }
+    foo.should == :bar
+    herp.should == :derp
+  end
+end
